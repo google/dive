96ba55d8aab7b3fad1f9215d9083075332d07831
wangra <wangra@google.com>
2023-10-31 16:17:42 -0400

    [mesa] Update freedreno python script to fix a warning of the generated header - we dont really care a2xx gpus, and the default value of ~0 is not correct anyways, so set the value to 0xffffffff to avoid the warning the prevent compiling the header

--- third_party/mesa/src/freedreno/common/freedreno_devices.py
+++ third_party/mesa/src/freedreno/common/freedreno_devices.py
@@ -204,7 +204,7 @@ add_gpus([
         gmem_align_w = 32,  gmem_align_h = 32,
         tile_align_w = 32,  tile_align_h = 32,
         tile_max_w   = 512,
-        tile_max_h   = ~0, # TODO
+        tile_max_h   = 0xffffffff, #GOOGLE, change this to 0xffffffff to fix a warning in generated header, original value: ~0, # TODO
         num_vsc_pipes = 8,
         cs_shared_mem_size = 0,
         num_sp_cores = 0, # TODO

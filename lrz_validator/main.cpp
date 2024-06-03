75d2767c2dc56e426c83270f47999ffc2f659af3
wangra <wangra@google.com>
2024-06-03 15:31:39 -0400

    output global lrz validation result and make outut file optional

--- lrz_validator/main.cpp
+++ lrz_validator/main.cpp
@@ -14,48 +14,36 @@
  limitations under the License.
 */
 #include <iostream>
+#include <optional>
 #include <vector>
 
 #include "dive_core/data_core.h"
 #include "pm4_info.h"
 
-int main(int argc, char **argv)
+bool ValidataLRZ(const Dive::CaptureMetadata &meta_data, const std::string &output_file_name)
 {
-    Pm4InfoInit();
-    if ((argc != 2) && (argc != 3))
-    {
-        std::cout
-        << "You need to call: lrz_validator <input_file_name.rd> <output_file_name.txt>(optional)";
-        return 0;
-    }
-    char *input_file_name = argv[1];
-
-    std::string output_file_name = "lrz_validation_result.txt";
-    if (argc == 3)
-    {
-        output_file_name = argv[2];
-    }
-
-    Dive::LogCompound               log_compound;
-    std::unique_ptr<Dive::DataCore> data_core = std::make_unique<Dive::DataCore>(&log_compound);
-    Dive::CaptureData::LoadResult   load_res = data_core->LoadCaptureData(input_file_name);
-    if (load_res != Dive::CaptureData::LoadResult::kSuccess)
-    {
-        std::cout << "Loading capture " << argv[1] << " failed!";
-        return 0;
-    }
-
-    if (!data_core->CreateMetaData())
-    {
-        std::cout << "Failed to create meta data!";
-        return 0;
-    }
-
-    std::cout << "Output validation result to " << output_file_name << std::endl;
-    std::ofstream                output_file(output_file_name);
-    const Dive::CaptureMetadata &meta_data = data_core->GetCaptureMetadata();
     size_t                       event_count = meta_data.m_event_info.size();
     const Dive::EventStateInfo  &event_state = meta_data.m_event_state;
+    bool                         lrz_test_passed = true;
+    std::optional<std::ofstream> output_file = std::nullopt;
+    if (!output_file_name.empty())
+    {
+        std::cout << "Output detailed validation result to \"" << output_file_name << "\""
+                  << std::endl;
+        output_file.emplace(std::ofstream(output_file_name));
+    }
+
+    std::function<void(const std::string &)> OutputDetails;
+
+    if (output_file.has_value())
+    {
+        OutputDetails = [&output_file](const std::string &str) { *output_file << str; };
+    }
+    else
+    {
+        OutputDetails = [](const std::string &) {};
+    }
+
     for (size_t i = 0; i < event_count; ++i)
     {
         const Dive::EventInfo &info = meta_data.m_event_info[i];
@@ -78,29 +66,16 @@ int main(int argc, char **argv)
             const uint32_t desired_draw_string_len = 64;
             std::string    draw_string = info.m_str;
             AppendSpace(draw_string, desired_draw_string_len);
-            output_file << draw_string + "\t";
+            OutputDetails(draw_string + "\t");
 
             const bool  is_depth_test_enabled = event_state_it->DepthTestEnabled();
             const bool  is_depth_write_enabled = event_state_it->DepthWriteEnabled();
             VkCompareOp zfunc = event_state_it->DepthCompareOp();
 
-            if (is_depth_test_enabled)
-            {
-                output_file << "DepthTest:Enabled\t";
-            }
-            else
-            {
-                output_file << "DepthTest:Disabled\t";
-            }
-
-            if (is_depth_write_enabled)
-            {
-                output_file << "DepthWrite:Enabled\t";
-            }
-            else
-            {
-                output_file << "DepthWrite:Disabled\t";
-            }
+            OutputDetails("DepthTest:" +
+                          std::string(is_depth_test_enabled ? "Enabled\t" : "Disabled\t"));
+            OutputDetails("DepthWrite:" +
+                          std::string(is_depth_write_enabled ? "Enabled\t" : "Disabled\t"));
 
             std::string zfunc_str = "Invalid";
             switch (zfunc)
@@ -117,33 +92,85 @@ int main(int argc, char **argv)
             }
             const uint32_t desired_zfunc_str_len = 16;
             AppendSpace(zfunc_str, desired_zfunc_str_len);
-            output_file << "DepthFunc:" + zfunc_str + "\t";
+            OutputDetails("DepthFunc:" + zfunc_str + "\t");
             if (is_depth_test_enabled)
             {
                 const bool lrz_enabled = event_state_it->LRZEnabled();
                 if (!lrz_enabled)
                 {
-                    output_file << "LRZ:Disabled\t";
+                    OutputDetails("LRZ:Disabled\t");
                     // if depth func is Always or Never, we don't really care about LRZ
                     if (is_depth_test_enabled &&
                         ((zfunc != VK_COMPARE_OP_NEVER) && (zfunc != VK_COMPARE_OP_ALWAYS)))
                     {
-                        output_file
-                        << "[WARNING!!!] LRZ is disabled for this drawcall but depth test is "
-                           "enabled, and depth func is not Never or Always!";
+                        lrz_test_passed = false;
+                        OutputDetails("[WARNING!] LRZ is disabled with performance penalties!");
                     }
                 }
                 else
                 {
-                    output_file << "LRZ:Enabled\t";
+                    OutputDetails("LRZ:Enabled\t");
                 }
             }
             else
             {
-                output_file << "LRZ:Disabled\t";
+                OutputDetails("LRZ:Disabled\t");
             }
-            output_file << std::endl;
+            OutputDetails("\n");
         }
+    }
+    return lrz_test_passed;
+}
+
+int main(int argc, char **argv)
+{
+    Pm4InfoInit();
+
+    // Handle args
+    if ((argc != 2) && (argc != 3))
+    {
+        std::cout << "You need to call: lrz_validator <input_file_name.rd> "
+                     "<output_details_file_name.txt>(optional)";
+        return 0;
+    }
+    char *input_file_name = argv[1];
+
+    std::string output_file_name = "";
+    if (argc == 3)
+    {
+        output_file_name = argv[2];
+    }
+
+    // Load capture
+    Dive::LogCompound               log_compound;
+    std::unique_ptr<Dive::DataCore> data_core = std::make_unique<Dive::DataCore>(&log_compound);
+    Dive::CaptureData::LoadResult   load_res = data_core->LoadCaptureData(input_file_name);
+    if (load_res != Dive::CaptureData::LoadResult::kSuccess)
+    {
+        std::cout << "Loading capture \"" << input_file_name << "\" failed!";
+        return 0;
+    }
+    std::cout << "Capture file \"" << input_file_name << "\" is loaded!\n";
+
+    // Create meta data
+    if (!data_core->CreateMetaData())
+    {
+        std::cout << "Failed to create meta data!";
+        return 0;
+    }
+    std::cout << "Validating LRZ...\n";
+
+    // LRZ Validation
+    const Dive::CaptureMetadata &meta_data = data_core->GetCaptureMetadata();
+    const bool                   lrz_test_passed = ValidataLRZ(meta_data, output_file_name);
+    if (lrz_test_passed)
+    {
+        std::cout << "[LRZ Pass] LRZ is correctly set for all drawcalls!\n";
+    }
+    else
+    {
+        std::cout << "[LRZ Fail] Some drawcalls have LRZ disabled but depth test is enabled and "
+                     "depth func is not set to NEVER or ALWAYS!\n";
     }
 
     return 1;

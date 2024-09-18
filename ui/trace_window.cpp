e98d07d2367069fdd94f123e8de5153f4c8b08f2
Grant <gcommodore@google.com>
2024-09-18 18:38:06 +0000

    Makes the packages combo box searchable

--- ui/trace_window.cpp
+++ ui/trace_window.cpp
@@ -29,6 +29,8 @@
 #include <QSortFilterProxyModel>
 #include <QStandardItem>
 #include <QStandardItemModel>
+#include <QSortFilterProxyModel>
+#include <QCompleter>
 #include <QThread>
 #include <QVBoxLayout>
 #include <cstdint>

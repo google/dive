87a221bd8610179579c1e27350e86627d88e540c
Grant Commodore <grantcommodore24@gmail.com>
2024-09-19 13:18:30 -0700

    Searchable QComboBox  (#134)
    * Makes the packages combo box searchable
    
    * Formatting

--- ui/trace_window.cpp
+++ ui/trace_window.cpp
@@ -17,6 +17,7 @@
 #include "trace_window.h"
 
 #include <QComboBox>
+#include <QCompleter>
 #include <QDebug>
 #include <QHBoxLayout>
 #include <QLabel>
@@ -25,6 +26,7 @@
 #include <QProgressDialog>
 #include <QPushButton>
 #include <QSizePolicy>
+#include <QSortFilterProxyModel>
 #include <QStandardItem>
 #include <QStandardItemModel>
 #include <QThread>
@@ -96,6 +98,13 @@ TraceDialog::TraceDialog(QWidget *parent)
     m_pkg_box->setMinimumSize(m_dev_box->sizeHint());
     m_pkg_box->setSizeAdjustPolicy(
     QComboBox::SizeAdjustPolicy::AdjustToMinimumContentsLengthWithIcon);
+    m_pkg_box->setEditable(true);
+    QSortFilterProxyModel *filterModel = new QSortFilterProxyModel(m_pkg_box);
+    filterModel->setFilterCaseSensitivity(Qt::CaseInsensitive);
+    filterModel->setSourceModel(m_pkg_box->model());
+    QCompleter *completer = new QCompleter(filterModel, m_pkg_box);
+    completer->setCompletionMode(QCompleter::UnfilteredPopupCompletion);
+    m_pkg_box->setCompleter(completer);
 
     m_app_type_box->setModel(m_app_type_model);
 
@@ -147,6 +156,10 @@ TraceDialog::TraceDialog(QWidget *parent)
                      SIGNAL(currentIndexChanged(const QString &)),
                      this,
                      SLOT(OnPackageSelected(const QString &)));
+    QObject::connect(m_pkg_box->lineEdit(),
+                     &QLineEdit::textEdited,
+                     filterModel,
+                     &QSortFilterProxyModel::setFilterFixedString);
     QObject::connect(m_run_button, &QPushButton::clicked, this, &TraceDialog::OnStartClicked);
     QObject::connect(m_capture_button, &QPushButton::clicked, this, &TraceDialog::OnTraceClicked);
 

57f32de52af844e063ab9af7504fcd2aa74bcaff
Angela Chen <chenangela@google.com>
2025-03-31 16:03:11 -0400

    Add feature for looping a single frame (#187)
    * Example usage: `python third_party\gfxreconstruct\android\scripts\gfxrecon.py replay --loop-single-frame --loop-n-times 500 <FILE>`
    * Since there doesn't seem to be a way to tell if a capture file contains multiple frames or not until reading through it, this change will loop the first frame of a multiframe capture for N times and then run through the remaining frames before terminating. This usage is discouraged and there will be an a error logged if detected.

---
+++ third_party/gfxreconstruct/framework/decode/dive_file_processor.cpp
@@ -0,0 +1,113 @@
+/*
+Copyright 2025 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Implementing a custom file processor for Dive
+
+#include "decode/dive_file_processor.h"
+#include "util/logging.h"
+
+GFXRECON_BEGIN_NAMESPACE(gfxrecon)
+GFXRECON_BEGIN_NAMESPACE(decode)
+
+// TODO GH #1195: frame numbering should be 1-based.
+const uint32_t kFirstFrame = 0;
+
+void DiveFileProcessor::SetLoopSingleFrameCount(uint64_t loop_single_frame_count)
+{
+    loop_single_frame_count_ = loop_single_frame_count;
+}
+
+bool DiveFileProcessor::ProcessFrameMarker(const format::BlockHeader& block_header,
+                                           format::MarkerType         marker_type,
+                                           bool&                      should_break)
+{
+    // Read the rest of the frame marker data. Currently frame markers are not dispatched to
+    // decoders.
+    uint64_t frame_number = 0;
+    bool     success      = ReadBytes(&frame_number, sizeof(frame_number));
+
+    if (success)
+    {
+        // Validate frame end marker's frame number matches first_frame_ when capture_uses_frame_markers_ is
+        // true.
+        GFXRECON_ASSERT((marker_type != format::kEndMarker) || (!UsesFrameMarkers()) ||
+                        (frame_number == GetFirstFrame()));
+
+        for (auto decoder : decoders_)
+        {
+            if (marker_type == format::kEndMarker)
+            {
+                decoder->DispatchFrameEndMarker(frame_number);
+            }
+            else
+            {
+                GFXRECON_LOG_WARNING("Skipping unrecognized frame marker with type %u", marker_type);
+            }
+        }
+    }
+    else
+    {
+        HandleBlockReadError(kErrorReadingBlockData, "Failed to read frame marker data");
+    }
+
+    // Break from loop on frame delimiter.
+    if (IsFrameDelimiter(block_header.type, marker_type))
+    {
+        // If the capture file contains frame markers, it will have a frame marker for every
+        // frame-ending API call such as vkQueuePresentKHR. If this is the first frame marker
+        // encountered, reset the frame count and ignore frame-ending API calls in
+        // IsFrameDelimiter(format::ApiCallId call_id).
+        if (!UsesFrameMarkers())
+        {
+            SetUsesFrameMarkers(true);
+            current_frame_number_ = kFirstFrame;
+        }
+
+        // Make sure to increment the frame number on the way out.
+        ++current_frame_number_;
+        ++block_index_;
+        should_break = true;
+
+        // At the last frame in the capture file, determine whether to jump back to the state end
+        // marker, or terminate replay if the loop count has been reached
+        if ((loop_single_frame_count_ > 0) && (current_frame_number_ >= loop_single_frame_count_))
+        {
+            GFXRECON_LOG_INFO("Looped %d frames, terminating replay asap", current_frame_number_);
+            return success;
+        }
+        SeekActiveFile(state_end_marker_file_offset_, util::platform::FileSeekSet);
+        should_break = false;
+    }
+    return success;
+}
+
+bool DiveFileProcessor::ProcessStateMarker(const format::BlockHeader& block_header, format::MarkerType marker_type)
+{
+    bool success = FileProcessor::ProcessStateMarker(block_header, marker_type);
+
+    if ((success) && (marker_type == format::kEndMarker))
+    {
+        // Store state end marker offset
+        state_end_marker_file_offset_ = TellActiveFile();
+        GFXRECON_LOG_INFO("Stored state end marker offset %d", state_end_marker_file_offset_);
+        GFXRECON_LOG_INFO("Single frame number %d", GetFirstFrame());
+    }
+
+    return success;
+}
+
+GFXRECON_END_NAMESPACE(decode)
+GFXRECON_END_NAMESPACE(gfxrecon)

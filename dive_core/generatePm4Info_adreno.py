825f21fb9ae94cb63b366c62b7704261666aacd5
wangra-google <62302956+wangra-google@users.noreply.github.com>
2025-05-14 14:30:03 -0400

    Update dive_core/generatePm4Info_adreno.py
    Co-authored-by: Michael Hitchens <hitchens@google.com>

--- dive_core/generatePm4Info_adreno.py
+++ dive_core/generatePm4Info_adreno.py
@@ -17,7 +17,7 @@ def outputHeader(pm4_info_file):
     
      Licensed under the Apache License, Version 2.0 (the "License");
      you may not use this file except in compliance with the License.
-     You may obtain a copy of the License at\n'
+     You may obtain a copy of the License at
     
      http://www.apache.org/licenses/LICENSE-2.0
     

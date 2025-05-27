f17169b3667a889879d4db260a2fc9c3f763bf3c
Elvis Capia <ecapia@google.com>
2025-05-27 02:18:59 -0400

    Communication protocol for Dive: Socket connection
    - The socket connection header and its implementation were added.
    - An initial cmake setup was added for transfer_client and
      transfer_server libraries.

---
+++ network/socket_connection.h
@@ -0,0 +1,94 @@
+/*
+Copyright 2025 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#pragma once
+
+#include <memory>
+#include <system_error>
+
+#include "absl/status/statusor.h"
+
+#ifdef WIN32
+// On Windows, a socket is a pointer-sized handle to ensure 32/64-bit compatibility.
+typedef uintptr_t SocketType;
+// The value for an invalid socket on Windows is INVALID_SOCKET (~0).
+constexpr SocketType kInvalidSocketValue = ~static_cast<SocketType>(0);
+#else
+// On POSIX systems, a socket is a file descriptor (`int`).
+typedef int SocketType;
+// Functions that return a file descriptor use -1 to indicate an error.
+constexpr SocketType kInvalidSocketValue = -1;
+#endif
+
+constexpr int kNoTimeout = -1;
+constexpr int kAcceptTimeout = 2000;
+
+namespace Network
+{
+
+class NetworkInitializer
+{
+public:
+    NetworkInitializer();
+    NetworkInitializer& operator=(const NetworkInitializer&) = delete;
+    NetworkInitializer(const NetworkInitializer&) = delete;
+
+    static const NetworkInitializer& Instance();
+    bool                             IsInitialized() const;
+
+private:
+    bool m_initialized;
+};
+
+class SocketConnection
+{
+public:
+    static absl::StatusOr<std::unique_ptr<SocketConnection>> Create(
+    SocketType initial_socket_value = kInvalidSocketValue);
+
+    ~SocketConnection();
+
+    SocketConnection& operator=(const SocketConnection&) = delete;
+    SocketConnection(const SocketConnection&) = delete;
+
+    // Server methods.
+    absl::Status BindAndListenOnUnixDomain(const std::string& server_address);
+    absl::StatusOr<std::unique_ptr<SocketConnection>> Accept();
+
+    // Client method.
+    absl::Status Connect(const std::string& host, int port);
+
+    // Data transfer methods.
+    absl::Status                Send(const uint8_t* data, size_t size);
+    absl::StatusOr<size_t>      Recv(uint8_t* data, size_t size);
+    absl::Status                SendString(const std::string& s);
+    absl::StatusOr<std::string> ReceiveString();
+    absl::Status                SendFile(const std::string& file_path);
+    absl::Status                ReceiveFile(const std::string& file_path, size_t file_size);
+
+    void Close();
+    bool IsOpen() const;
+
+private:
+    explicit SocketConnection(SocketType initial_socket_value);
+
+    SocketType m_socket;
+    bool       m_is_listening;
+    int        m_accept_timout_ms;
+    int        m_recv_timeout_ms;
+};
+
+}  // namespace Network

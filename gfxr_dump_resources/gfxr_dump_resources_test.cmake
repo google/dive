6b9ddb7b88d80f16c47cc067867e44a7e0eb3231
Michael Hitchens <hitchens@google.com>
2025-06-20 14:15:58 -0400

    normalize newlines so that compare_files is cross platform

--- gfxr_dump_resources/gfxr_dump_resources_test.cmake
+++ gfxr_dump_resources/gfxr_dump_resources_test.cmake
@@ -9,14 +9,22 @@ execute_process(
   COMMAND ${TEST_EXECUTABLE} ${PROJECT_SOURCE_DIR}/tests/gfxr_traces/vs_triangle_300_20221211T232110.gfxr test_output.json
   RESULT_VARIABLE exit_code
 )
+# In CMake 3.19+, prefer COMMAND_ERROR_IS_FATAL (more succinct)
 if (NOT exit_code EQUAL 0)
   message(FATAL_ERROR "${TEST_EXECUTABLE} failed: ${exit_code}")
 endif()
 
+# Since git lets users specify newline style, normalize both input and output to compare_files.
+# This is required so that compare_files works regardless of platform and setting.
+# In CMake 3.14+, prefer compare_files --ignore-eol (more succinct)
+configure_file(test_output.json test_output_unix_newline.json NEWLINE_STYLE UNIX)
+configure_file(${PROJECT_SOURCE_DIR}/tests/golden/vs_triangle_300_20221211T232110_dump_resources_golden.json golden_unix_newline.json NEWLINE_STYLE UNIX)
+
 execute_process(
-  COMMAND ${CMAKE_COMMAND} -E compare_files ${PROJECT_SOURCE_DIR}/tests/golden/vs_triangle_300_20221211T232110_dump_resources_golden.json test_output.json
+  COMMAND ${CMAKE_COMMAND} -E compare_files golden_unix_newline.json test_output_unix_newline.json
   RESULT_VARIABLE exit_code
 )
+# In CMake 3.19+, prefer COMMAND_ERROR_IS_FATAL (more succinct)
 if (NOT exit_code EQUAL 0)
   message(FATAL_ERROR "Output differs from golden file")
 endif()

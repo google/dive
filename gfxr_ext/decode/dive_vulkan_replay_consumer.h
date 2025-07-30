3a85414f822b047bdb873f6b7d3982de6cb64f86
wangra <wangra@google.com>
2025-07-30 11:13:54 -0400

    Minor fixes

--- gfxr_ext/decode/dive_vulkan_replay_consumer.h
+++ gfxr_ext/decode/dive_vulkan_replay_consumer.h
@@ -18,7 +18,7 @@ limitations under the License.
 #define GFXRECON_DECODE_VULKAN_DIVE_CONSUMER_H
 
 #include "generated/generated_vulkan_replay_consumer.h"
-#include "gpu_time.h"
+#include "gpu_time/gpu_time.h"
 #include <set>
 #include <vector>
 
@@ -33,13 +33,12 @@ public:
 
     ~DiveVulkanReplayConsumer() override;
 
-    void Process_vkCreateDevice(
-    const ApiCallInfo&                                   call_info,
-    VkResult                                             returnValue,
-    format::HandleId                                     physicalDevice,
-    StructPointerDecoder<Decoded_VkDeviceCreateInfo>*    pCreateInfo,
-    StructPointerDecoder<Decoded_VkAllocationCallbacks>* pAllocator,
-    HandlePointerDecoder<VkDevice>*                      pDevice) override;
+    void Process_vkCreateDevice(const ApiCallInfo&                                   call_info,
+                                VkResult                                             returnValue,
+                                format::HandleId                                     physicalDevice,
+                                StructPointerDecoder<Decoded_VkDeviceCreateInfo>*    pCreateInfo,
+                                StructPointerDecoder<Decoded_VkAllocationCallbacks>* pAllocator,
+                                HandlePointerDecoder<VkDevice>* pDevice) override;
 
     void Process_vkDestroyDevice(
     const ApiCallInfo&                                   call_info,
@@ -67,15 +66,15 @@ public:
     HandlePointerDecoder<VkCommandBuffer>* pCommandBuffers) override;
 
     void Process_vkResetCommandBuffer(const ApiCallInfo&        call_info,
-                                              VkResult                  returnValue,
-                                              format::HandleId          commandBuffer,
-                                              VkCommandBufferResetFlags flags) override;
+                                      VkResult                  returnValue,
+                                      format::HandleId          commandBuffer,
+                                      VkCommandBufferResetFlags flags) override;
 
     void Process_vkResetCommandPool(const ApiCallInfo&      call_info,
-                                            VkResult                returnValue,
-                                            format::HandleId        device,
-                                            format::HandleId        commandPool,
-                                            VkCommandPoolResetFlags flags) override;
+                                    VkResult                returnValue,
+                                    format::HandleId        device,
+                                    format::HandleId        commandPool,
+                                    VkCommandPoolResetFlags flags) override;
 
     void Process_vkQueueSubmit(const ApiCallInfo&                          call_info,
                                VkResult                                    returnValue,
@@ -91,20 +90,19 @@ public:
     StructPointerDecoder<Decoded_VkCommandBufferBeginInfo>* pBeginInfo) override;
 
     void Process_vkEndCommandBuffer(const ApiCallInfo& call_info,
-                                            VkResult           returnValue,
-                                            format::HandleId   commandBuffer) override;
+                                    VkResult           returnValue,
+                                    format::HandleId   commandBuffer) override;
 
-    void Process_vkGetDeviceQueue2(
-    const ApiCallInfo&                                call_info,
-    format::HandleId                                  device,
-    StructPointerDecoder<Decoded_VkDeviceQueueInfo2>* pQueueInfo,
-    HandlePointerDecoder<VkQueue>*                    pQueue) override;
+    void Process_vkGetDeviceQueue2(const ApiCallInfo&                                call_info,
+                                   format::HandleId                                  device,
+                                   StructPointerDecoder<Decoded_VkDeviceQueueInfo2>* pQueueInfo,
+                                   HandlePointerDecoder<VkQueue>* pQueue) override;
 
     void Process_vkGetDeviceQueue(const ApiCallInfo&             call_info,
-                                          format::HandleId               device,
-                                          uint32_t                       queueFamilyIndex,
-                                          uint32_t                       queueIndex,
-                                          HandlePointerDecoder<VkQueue>* pQueue) override;
+                                  format::HandleId               device,
+                                  uint32_t                       queueFamilyIndex,
+                                  uint32_t                       queueIndex,
+                                  HandlePointerDecoder<VkQueue>* pQueue) override;
 
     void Process_vkCmdInsertDebugUtilsLabelEXT(
     const ApiCallInfo&                                  call_info,

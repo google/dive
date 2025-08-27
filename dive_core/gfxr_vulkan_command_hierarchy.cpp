520a2d537b250a7ba324e08e24a70905c14f146d
Grant <gcommodore@google.com>
2025-08-27 20:56:20 +0000

    Adds vulkan_cmd_name var

--- dive_core/gfxr_vulkan_command_hierarchy.cpp
+++ dive_core/gfxr_vulkan_command_hierarchy.cpp
@@ -51,40 +51,42 @@ void GfxrVulkanCommandHierarchyCreator::OnCommand(
 uint32_t                                   parent_index,
 DiveAnnotationProcessor::VulkanCommandInfo vk_cmd_info)
 {
-    std::ostringstream vk_cmd_string_stream;
-    vk_cmd_string_stream << vk_cmd_info.GetVkCmdName();
-    if (vk_cmd_info.GetVkCmdName() == "vkBeginCommandBuffer")
+    const std::string             vulkan_cmd_name = vk_cmd_info.GetVkCmdName();
+    const nlohmann::ordered_json &vulkan_cmd_args = vk_cmd_info.GetArgs();
+    std::ostringstream            vk_cmd_string_stream;
+    vk_cmd_string_stream << vulkan_cmd_name;
+    if (vulkan_cmd_name == "vkBeginCommandBuffer")
     {
         uint64_t cmd_buffer_index = AddNode(NodeType::kGfxrVulkanCommandBufferNode,
                                             vk_cmd_string_stream.str());
         m_cur_command_buffer_node_index = cmd_buffer_index;
-        GetArgs(vk_cmd_info.GetArgs(), m_cur_command_buffer_node_index, "");
+        GetArgs(vulkan_cmd_args, m_cur_command_buffer_node_index, "");
         AddChild(CommandHierarchy::TopologyType::kAllEventTopology,
                  m_cur_submit_node_index,
                  cmd_buffer_index);
     }
-    else if (vk_cmd_info.GetVkCmdName() == "vkEndCommandBuffer")
+    else if (vulkan_cmd_name == "vkEndCommandBuffer")
     {
         uint64_t cmd_buffer_index = AddNode(NodeType::kGfxrVulkanCommandBufferNode,
                                             vk_cmd_string_stream.str());
 
-        GetArgs(vk_cmd_info.GetArgs(), m_cur_command_buffer_node_index, "");
+        GetArgs(vulkan_cmd_args, m_cur_command_buffer_node_index, "");
         AddChild(CommandHierarchy::TopologyType::kAllEventTopology,
                  m_cur_submit_node_index,
                  cmd_buffer_index);
     }
-    else if (vk_cmd_info.GetVkCmdName().find("BeginDebugUtilsLabelEXT") != std::string::npos)
+    else if (vulkan_cmd_name.find("BeginDebugUtilsLabelEXT") != std::string::npos)
     {
-        std::string label_name = vk_cmd_info.GetArgs()["pLabelInfo"]["pLabelName"];
+        std::string label_name = vulkan_cmd_args["pLabelInfo"]["pLabelName"];
 
         uint64_t
         begin_debug_utils_label_cmd_index = AddNode(NodeType::kGfxrBeginDebugUtilsLabelCommandNode,
                                                     label_name.c_str());
-        GetArgs(vk_cmd_info.GetArgs(), begin_debug_utils_label_cmd_index, "");
+        GetArgs(vulkan_cmd_args, begin_debug_utils_label_cmd_index, "");
         ConditionallyAddChild(begin_debug_utils_label_cmd_index);
         m_cur_begin_debug_utils_node_index_stack.push(begin_debug_utils_label_cmd_index);
     }
-    else if (vk_cmd_info.GetVkCmdName().find("EndDebugUtilsLabelEXT") != std::string::npos)
+    else if (vulkan_cmd_name.find("EndDebugUtilsLabelEXT") != std::string::npos)
     {
         if (!m_cur_begin_debug_utils_node_index_stack.empty())
         {
@@ -92,26 +94,26 @@ DiveAnnotationProcessor::VulkanCommandInfo vk_cmd_info)
             m_cur_begin_debug_utils_node_index_stack.pop();
         }
     }
-    else if (vk_cmd_info.GetVkCmdName().find("vkCmdDraw") != std::string::npos ||
-             vk_cmd_info.GetVkCmdName().find("vkCmdDispatch") != std::string::npos)
+    else if (vulkan_cmd_name.find("vkCmdDraw") != std::string::npos ||
+             vulkan_cmd_name.find("vkCmdDispatch") != std::string::npos)
     {
         uint64_t vk_cmd_index = AddNode(NodeType::kGfxrVulkanDrawCommandNode,
                                         vk_cmd_string_stream.str());
-        GetArgs(vk_cmd_info.GetArgs(), vk_cmd_index, "");
+        GetArgs(vulkan_cmd_args, vk_cmd_index, "");
         ConditionallyAddChild(vk_cmd_index);
     }
-    else if (vk_cmd_info.GetVkCmdName().find("RenderPass") != std::string::npos)
+    else if (vulkan_cmd_name.find("RenderPass") != std::string::npos)
     {
         uint64_t vk_cmd_index = AddNode(NodeType::kGfxrVulkanRenderPassCommandNode,
                                         vk_cmd_string_stream.str());
-        GetArgs(vk_cmd_info.GetArgs(), vk_cmd_index, "");
+        GetArgs(vulkan_cmd_args, vk_cmd_index, "");
         ConditionallyAddChild(vk_cmd_index);
     }
     else
     {
         uint64_t vk_cmd_index = AddNode(NodeType::kGfxrVulkanCommandNode,
                                         vk_cmd_string_stream.str());
-        GetArgs(vk_cmd_info.GetArgs(), vk_cmd_index, "");
+        GetArgs(vulkan_cmd_args, vk_cmd_index, "");
         ConditionallyAddChild(vk_cmd_index);
     }
 }

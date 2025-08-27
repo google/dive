dc9e777b5bfaac13dfa2f1c41c98b88c22940843
Angela Chen <chenangela@google.com>
2025-08-27 14:49:10 -0400

    Add functionality for outputting GPU timing stats to a file (#338)
    When GPU timing data is collected with flag `--enable-gpu-time`, the
    stats will be output to file `gpu_time.csv` located in the same dir as
    the .gfxr capture.
    
    Other changes:
    - Index fix for GFXR traces with command buffers containing multiple
    renderpasses

--- gfxr_ext/decode/dive_file_processor.cpp
+++ gfxr_ext/decode/dive_file_processor.cpp
@@ -19,6 +19,7 @@ limitations under the License.
 #include "dive_file_processor.h"
 
 #include "util/logging.h"
+#include "util/platform.h"
 
 #include "dive_block_data.h"
 #include "dive_pm4_capture.h"
@@ -40,6 +41,36 @@ void DiveFileProcessor::SetDiveBlockData(std::shared_ptr<DiveBlockData> p_block_
 
     // When populating DiveBlockData we want to run through the entire file.
     run_without_decoders_ = true;
+}
+
+bool DiveFileProcessor::WriteFile(const std::string& name, const std::string& content)
+{
+    std::string new_file_path = absolute_path_ + "/" + name;
+
+    FILE* fd;
+    int   result = util::platform::FileOpen(&fd, new_file_path.c_str(), "wb");
+    if (result || fd == nullptr)
+    {
+        GFXRECON_LOG_ERROR("Failed to open file %s, exit code: %d", new_file_path.c_str(), result);
+        return false;
+    }
+
+    bool res = util::platform::FilePuts(content.c_str(), fd);
+    if (!res)
+    {
+        GFXRECON_LOG_ERROR("Could not write file: %s", new_file_path.c_str());
+    }
+
+    GFXRECON_LOG_INFO("Wrote file: %s", new_file_path.c_str());
+
+    result = util::platform::FileClose(fd);
+    if (result)
+    {
+        GFXRECON_LOG_ERROR("Failed to close file %s, exit code: %d", new_file_path.c_str(), result);
+        return false;
+    }
+
+    return true;
 }
 
 bool DiveFileProcessor::ProcessFrameMarker(const format::BlockHeader& block_header,

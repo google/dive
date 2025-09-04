35805de23b8ee52c35685644ede643e342affde5
Robin Liu <renfeng@google.com>
2025-09-04 14:16:45 -0400

    Apply review suggestions

--- dive_core/tests/perf_metrics_data_test.cpp
+++ dive_core/tests/perf_metrics_data_test.cpp
@@ -17,14 +17,40 @@
 #include "dive_core/perf_metrics_data.h"
 #include "dive_core/available_metrics.h"
 #include "gtest/gtest.h"
-#include <filesystem>
 #include "gmock/gmock.h"
 
 namespace Dive
 {
 
+void PrintTo(const PerfMetricsRecord& record, std::ostream* os)
+{
+    *os << "PerfMetricsRecord {" << std::endl;
+    *os << "  m_context_id: " << record.m_context_id << "," << std::endl;
+    *os << "  m_process_id: " << record.m_process_id << "," << std::endl;
+    *os << "  m_frame_id: " << record.m_frame_id << "," << std::endl;
+    *os << "  m_cmd_buffer_id: " << record.m_cmd_buffer_id << "," << std::endl;
+    *os << "  m_draw_id: " << record.m_draw_id << "," << std::endl;
+    *os << "  m_draw_label: " << record.m_draw_label << "," << std::endl;
+    *os << "  m_program_id: " << record.m_program_id << "," << std::endl;
+    *os << "  m_draw_type: " << static_cast<int>(record.m_draw_type) << "," << std::endl;
+    *os << "  m_lrz_state: " << static_cast<int>(record.m_lrz_state) << "," << std::endl;
+    *os << "  m_metric_values: [";
+    for (size_t i = 0; i < record.m_metric_values.size(); ++i)
+    {
+        std::visit([os](auto&& arg) { *os << arg; }, record.m_metric_values[i]);
+        if (i < record.m_metric_values.size() - 1)
+        {
+            *os << ", ";
+        }
+    }
+    *os << "]" << std::endl;
+    *os << "}";
+}
+
 namespace
 {
+
+using ::testing::IsEmpty;
 
 MATCHER_P(PerfMetricsRecordEq, expected, "has the correct perf metrics record fields")
 {
@@ -40,24 +66,16 @@ MATCHER_P(PerfMetricsRecordEq, expected, "has the correct perf metrics record fi
     return true;
 }
 
-class PerfMetricsDataTest : public ::testing::Test
+TEST(PerfMetricsData, LoadFromCsv)
 {
-protected:
-    void SetUp() override
-    {
-        m_available_metrics = Dive::AvailableMetrics::LoadFromCsv(TEST_DATA_DIR
-                                                                  "/mock_available_metrics.csv");
-        ASSERT_TRUE(m_available_metrics);
-    }
-    std::optional<Dive::AvailableMetrics> m_available_metrics;
-};
+    auto available_metrics = Dive::AvailableMetrics::LoadFromCsv(TEST_DATA_DIR
+                                                                 "/mock_available_metrics.csv");
+    ASSERT_TRUE(available_metrics);
 
-TEST_F(PerfMetricsDataTest, LoadFromCsv)
-{
     auto perf_metrics_data = Dive::PerfMetricsData::LoadFromCsv(TEST_DATA_DIR
                                                                 "/mock_perf_metrics_data.csv",
-                                                                *m_available_metrics);
-    ASSERT_TRUE(perf_metrics_data);
+                                                                *available_metrics);
+    ASSERT_NE(perf_metrics_data, nullptr);
 
     const auto& records = perf_metrics_data->GetRecords();
     ASSERT_EQ(records.size(), 2);
@@ -109,47 +127,58 @@ TEST_F(PerfMetricsDataTest, LoadFromCsv)
     EXPECT_EQ(metric_infos[1]->m_metric_id, 2);
 }
 
-TEST_F(PerfMetricsDataTest, LoadFromCsvMalformedRowSkipped)
+TEST(PerfMetricsData, LoadFromCsvMalformedRowSkipped)
 {
+    auto available_metrics = AvailableMetrics::LoadFromCsv(
+    TEST_DATA_DIR "/mock_available_metrics_unknown_type.csv");
+    ASSERT_TRUE(available_metrics);
+
     auto
     perf_metrics_data = Dive::PerfMetricsData::LoadFromCsv(TEST_DATA_DIR
                                                            "/mock_perf_metrics_data_malformed.csv",
-                                                           *m_available_metrics);
+                                                           *available_metrics);
     ASSERT_TRUE(perf_metrics_data);
     const auto& records = perf_metrics_data->GetRecords();
     ASSERT_EQ(records.size(), 1);
 }
 
-TEST_F(PerfMetricsDataTest, LoadFromCsvFailedWithNoHeader)
+TEST(PerfMetricsData, LoadFromCsvFailedWithNoHeader)
 {
+    auto available_metrics = AvailableMetrics::LoadFromCsv(
+    TEST_DATA_DIR "/mock_available_metrics_unknown_type.csv");
+    ASSERT_TRUE(available_metrics);
+
     auto
     perf_metrics_data = Dive::PerfMetricsData::LoadFromCsv(TEST_DATA_DIR
                                                            "/mock_perf_metrics_data_no_header.csv",
-                                                           *m_available_metrics);
+                                                           *available_metrics);
     ASSERT_FALSE(perf_metrics_data);
 }
 
-TEST_F(PerfMetricsDataTest, LoadFromCsvWrongColumnsSkipped)
+TEST(PerfMetricsData, LoadFromCsvWrongColumnsSkipped)
 {
-    auto perf_metrics_data = Dive::PerfMetricsData::
-    LoadFromCsv(TEST_DATA_DIR "/mock_perf_metrics_data_wrong_columns.csv", *m_available_metrics);
-    ASSERT_TRUE(perf_metrics_data);
-    const auto& records = perf_metrics_data->GetRecords();
-    ASSERT_EQ(records.size(), 0);
-}
-
-TEST(PerfMetricsData, LoadFromCsvUnknownMeticTypeSkipped)
-{
-    auto available_metrics = Dive::AvailableMetrics::LoadFromCsv(
+    auto available_metrics = AvailableMetrics::LoadFromCsv(
     TEST_DATA_DIR "/mock_available_metrics_unknown_type.csv");
     ASSERT_TRUE(available_metrics);
 
     auto perf_metrics_data = Dive::PerfMetricsData::
-    LoadFromCsv(TEST_DATA_DIR "/mock_perf_metrics_data_unknown_type.csv", *available_metrics);
-    ASSERT_TRUE(perf_metrics_data);
+    LoadFromCsv(TEST_DATA_DIR "/mock_perf_metrics_data_wrong_columns.csv", *available_metrics);
+    ASSERT_NE(perf_metrics_data, nullptr);
+    ASSERT_THAT(perf_metrics_data->GetRecords(), IsEmpty());
+}
 
-    const auto& records = perf_metrics_data->GetRecords();
-    ASSERT_EQ(records.size(), 0);
+TEST(PerfMetricsData, LoadFromCsvUnknownMeticTypeSkipped)
+{
+    auto available_metrics = AvailableMetrics::LoadFromCsv(
+    TEST_DATA_DIR "/mock_available_metrics_unknown_type.csv");
+    ASSERT_TRUE(available_metrics);
+
+    auto
+    perf_metrics_data = PerfMetricsData::LoadFromCsv(TEST_DATA_DIR
+                                                     "/mock_perf_metrics_data_unknown_type.csv",
+                                                     *available_metrics);
+    ASSERT_NE(perf_metrics_data, nullptr);
+    ASSERT_THAT(perf_metrics_data->GetRecords(), IsEmpty());
 }
 }  // namespace
 

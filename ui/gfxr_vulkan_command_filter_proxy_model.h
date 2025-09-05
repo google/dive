28f4f9d0a6812efa7481e5da37bbad20283e43c2
Grant <gcommodore@google.com>
2025-09-05 22:16:21 +0000

    Adds comment for CollectGfxrDrawCallIndices

--- ui/gfxr_vulkan_command_filter_proxy_model.h
+++ ui/gfxr_vulkan_command_filter_proxy_model.h
@@ -39,6 +39,11 @@ public:
     GfxrVulkanCommandFilterProxyModel(const Dive::CommandHierarchy &command_hierarchy,
                                       QObject                      *parent = nullptr);
     void SetFilter(FilterMode filter_mode);
+
+    // The default parent index is QModelIndex() so that the top-level items (submits) are recursed
+    // through. When the sourceModel()->rowCount is called on QModelIndex(), the row count is the
+    // number of those top-level items. In the initial call, QModelIndex() is not valid so the list
+    // of indices is cleared. This is so we don't have any of the previous collected indices.
     void CollectGfxrDrawCallIndices(const QModelIndex &parent_index = QModelIndex());
     const std::vector<uint64_t> &GetGfxrDrawCallIndices() { return m_gfxr_draw_call_indices; }
 

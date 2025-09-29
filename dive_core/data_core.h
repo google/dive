5643bc616ce8f10fe9d470cb9241e539925a807a
Grant <gcommodore@google.com>
2025-09-29 07:51:24 +0000

    Moves trace stats collection and printing to dive_core

--- dive_core/data_core.h
+++ dive_core/data_core.h
@@ -30,6 +30,73 @@
 namespace Dive
 {
 
+#define CHECK_AND_TRACK_STATE_1(stats_enum, state)                   \
+    if (event_state_it->Is##state##Set() && event_state_it->state()) \
+        stats_list[stats_enum]++;
+
+#define CHECK_AND_TRACK_STATE_2(stats_enum, state1, state2)                       \
+    if (event_state_it->Is##state1##Set() && event_state_it->Is##state2##Set() && \
+        event_state_it->state1() && event_state_it->state2())                     \
+        stats_list[stats_enum]++;
+
+#define CHECK_AND_TRACK_STATE_3(stats_enum, state1, state2, state3)               \
+    if (event_state_it->Is##state1##Set() && event_state_it->Is##state2##Set() && \
+        event_state_it->Is##state3##Set() && event_state_it->state1() &&          \
+        event_state_it->state2() && event_state_it->state3())                     \
+        stats_list[stats_enum]++;
+
+#define CHECK_AND_TRACK_STATE_EQUAL(stats_enum, state, state_value) \
+    if (event_state_it->Is##state##Set())                           \
+        if (event_state_it->state() == state_value)                 \
+            stats_list[stats_enum]++;
+
+#define CHECK_AND_TRACK_STATE_NOT_EQUAL(stats_enum, state, state_value) \
+    if (event_state_it->Is##state##Set())                               \
+        if (event_state_it->state() != state_value)                     \
+            stats_list[stats_enum]++;
+
+#define FUNC_CHOOSER(_f1, _f2, _f3, _f4, ...) _f4
+#define MSVC_WORKAROUND(argsWithParentheses) FUNC_CHOOSER argsWithParentheses
+
+// Trailing , is workaround for GCC/CLANG, suppressing false positive error "ISO C99 requires rest
+// arguments to be used" MSVC_WORKAROUND is workaround for MSVC for counting # of arguments
+// correctly
+#define CHECK_AND_TRACK_STATE_N(...) \
+    MSVC_WORKAROUND(                 \
+    (__VA_ARGS__, CHECK_AND_TRACK_STATE_3, CHECK_AND_TRACK_STATE_2, CHECK_AND_TRACK_STATE_1, ))
+#define CHECK_AND_TRACK_STATE(stats_enum, ...) \
+    CHECK_AND_TRACK_STATE_N(__VA_ARGS__)(stats_enum, __VA_ARGS__)
+
+#define GATHER_TOTAL_MIN_MAX_MEDIAN(array_name, type)                                         \
+    {                                                                                         \
+        std::sort(array_name.begin(), array_name.end());                                      \
+        size_t n = array_name.size();                                                         \
+        if (n % 2 != 0)                                                                       \
+        {                                                                                     \
+            stats_list[Dive::Stats::kMedian##type] = array_name[n / 2];                       \
+        }                                                                                     \
+        else                                                                                  \
+        {                                                                                     \
+            auto mid1 = array_name[n / 2 - 1];                                                \
+            auto mid2 = array_name[n / 2];                                                    \
+            stats_list[Dive::Stats::kMedian##type] = (uint64_t)((float)(mid1 + mid2) / 2.0f); \
+        }                                                                                     \
+        stats_list[Dive::Stats::kMin##type] = *std::min_element(array_name.begin(),           \
+                                                                array_name.end());            \
+        stats_list[Dive::Stats::kMax##type] = *std::max_element(array_name.begin(),           \
+                                                                array_name.end());            \
+        stats_list[Dive::Stats::kTotal##type] = std::accumulate(array_name.begin(),           \
+                                                                array_name.end(),             \
+                                                                (uint64_t)0);                 \
+    }
+
+#define GATHER_RESOLVES(type)                         \
+    do                                                \
+    {                                                 \
+        stats_list[Dive::Stats::kTotalResolves]++;    \
+        stats_list[Dive::Stats::k##type##Resolves]++; \
+    } while (0)
+
 //--------------------------------------------------------------------------------------------------
 // Metadata that describes information in the capture
 struct CaptureMetadata
@@ -52,6 +119,65 @@ struct CaptureMetadata
 
     // Information about the submits in this capture
     uint64_t m_num_pm4_packets;
+};
+
+struct Viewport
+{
+    VkViewport m_vk_viewport;
+    bool       operator<(const Viewport &other) const
+    {
+        if (m_vk_viewport.x != other.m_vk_viewport.x)
+            return m_vk_viewport.x < other.m_vk_viewport.x;
+        if (m_vk_viewport.y != other.m_vk_viewport.y)
+            return m_vk_viewport.y < other.m_vk_viewport.y;
+        if (m_vk_viewport.width != other.m_vk_viewport.width)
+            return m_vk_viewport.width < other.m_vk_viewport.width;
+        if (m_vk_viewport.height != other.m_vk_viewport.height)
+            return m_vk_viewport.height < other.m_vk_viewport.height;
+        if (m_vk_viewport.minDepth != other.m_vk_viewport.minDepth)
+            return m_vk_viewport.minDepth < other.m_vk_viewport.minDepth;
+        return m_vk_viewport.maxDepth < other.m_vk_viewport.maxDepth;
+    }
+};
+
+struct WindowScissor
+{
+    uint32_t m_tl_x;
+    uint32_t m_tl_y;
+    uint32_t m_br_x;
+    uint32_t m_br_y;
+    bool     operator<(const WindowScissor &other) const
+    {
+        uint32_t area = (m_br_x - m_tl_x) * (m_br_y - m_tl_y);
+        uint32_t other_area = (other.m_br_x - other.m_tl_x) * (other.m_br_y - other.m_tl_y);
+        if (area != other_area)
+            return area < other_area;
+        if (m_tl_y != other.m_tl_y)
+            return m_tl_y < other.m_tl_y;
+        if (m_tl_x != other.m_tl_x)
+            return m_tl_x < other.m_tl_x;
+        if (m_br_y != other.m_br_y)
+            return m_br_y < other.m_br_y;
+        return m_br_x < other.m_br_x;
+    }
+};
+
+// ---------------------------------------------------------------------
+// Statistics Container
+// ---------------------------------------------------------------------
+
+struct CaptureStats
+{
+    std::array<uint64_t, Dive::Stats::kNumStats> stats_list = {};
+
+    std::vector<uint32_t> event_num_indices;
+
+    std::vector<Dive::ShaderReference> shader_ref_set;
+    std::vector<Viewport>              viewports;
+    std::vector<WindowScissor>         window_scissors;
+
+    uint32_t num_binning_passes = 0;
+    uint32_t num_tiling_passes = 0;
 };
 
 //--------------------------------------------------------------------------------------------------
@@ -97,6 +223,15 @@ public:
     // Get metadata describing the capture (info obtained by parsing the capture)
     const CaptureMetadata &GetCaptureMetadata() const;
 
+    // Gather various statistics from the capture metadata
+    void GatherCaptureStats();
+
+    // Print out the statistics to the provided output stream
+    void PrintCaptureStats(std::ostream &ostream);
+
+    // Get the capture statistics
+    const CaptureStats &GetCaptureStats() const;
+
 private:
     // Create command hierarchy from the captured data
     bool CreateDiveCommandHierarchy();
@@ -111,6 +246,9 @@ private:
 
     // Metadata for the capture data in m_capture_data
     CaptureMetadata m_capture_metadata;
+
+    // Statistics about the capture
+    CaptureStats m_capture_stats;
 };
 
 #if defined(ENABLE_CAPTURE_BUFFERS)

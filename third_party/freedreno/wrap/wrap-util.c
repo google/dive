79f8490f52b8895d09f486931ec3e83d19d64d49
Michael Hitchens <hitchens@google.com>
2025-09-30 12:11:35 -0400

    Revert "libwrap: Simplify collect_trace_file() (#407)" (#417)
    This reverts commit bcf2a8e0447a3bdf743cbbad4bac3a8818551ad8.
    
    Resolves an issue where libwrap would crash after trace was collected
    and was being transferred.

--- third_party/freedreno/wrap/wrap-util.c
+++ third_party/freedreno/wrap/wrap-util.c
@@ -529,7 +529,13 @@ int append_file(int fd_target, const char *src)
 // In the process, all log files are closed and all inprogress files are deleted.
 void collect_trace_file(const char* capture_file_path)
 {
-	LOGD("capture_file_path is %s", capture_file_path);
+	char full_path[PATH_MAX];
+#if defined (COMPRESS_TRACE)
+	snprintf(full_path, PATH_MAX, "%s", capture_file_path);
+#else
+	snprintf(full_path, PATH_MAX, "%s", capture_file_path);
+#endif
+	LOGD("full_path is %s", full_path);
 
 	// Could remove(capture_file_path). However, that is susceptible to TOCTOU problems.
 	// Prefer to open the target file once and keep it open for the duration of collection.
@@ -542,30 +548,30 @@ void collect_trace_file(const char* capture_file_path)
 	for (int i = 0; i < MAX_DEVICE_FILES; i++)
 	{
 		struct device_file* df = &device_files[i];
-		int fd = df->device_fd;
-		if(fd == -1 || df->log_fd == LOG_NULL_FILE)
+		int fd = 	df->device_fd ;
+		if(fd != -1)
 		{
-			continue;
-		}
-		LOGD("device_fd %d, log_fd %"LOG_PRI_FILE", filename %s closed in collect_trace_file \n",
-			fd, df->log_fd, df->file_name);
-		pthread_mutex_lock(&write_lock);
-		LOG_CLOSE_FILE(df->log_fd);
-		df->log_fd = LOG_NULL_FILE;
-		df->device_fd = -1;
-		pthread_mutex_unlock(&write_lock);
-		if(-1 == append_file(concatenated_log_fd, df->file_name))
-		{
-			LOGI("Failed to append file %s to target file %s\n", df->file_name, capture_file_path);
-		}
-		else
-		{
+			struct device_file *df = get_file(fd);
+			if (df == NULL)
+					continue;
+			LOGD("device_fd %d, log_fd %"LOG_PRI_FILE", filename %s closed in collect_trace_file \n",
+				fd, df->log_fd, df->file_name);
+			pthread_mutex_lock(&write_lock);
+			LOG_CLOSE_FILE(df->log_fd);
+			df->log_fd = LOG_NULL_FILE;
+			pthread_mutex_unlock(&write_lock);
+			if(-1 == append_file(concatenated_log_fd, df->file_name))
+			{
+				LOGI("Failed to append file %s to target file %s\n", df->file_name, capture_file_path);
+			}
+			// delete the file that has been concatenated. 
 			remove(df->file_name);
-		}
+        }
 	}
 
 	if (close(concatenated_log_fd) == -1) {
 		LOGI("Failed to close concatenated trace file `%s`. There might be some traces missing. Error: %s", capture_file_path, strerror(errno));
+			
 	}
 }
 void hexdump(const void *data, int size)

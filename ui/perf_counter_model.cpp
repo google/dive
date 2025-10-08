25e56a89f12146e950b3d67a57736e2a7ca26420
Grant Commodore <grantcommodore24@gmail.com>
2025-10-08 14:55:04 -0700

    Adds Perf Counter Sorting (#438)
    - Adds sorting of perf counters when clicking headers
    - Adds button to reset sorting order to default unordered

--- ui/perf_counter_model.cpp
+++ ui/perf_counter_model.cpp
@@ -160,7 +160,11 @@ QVariant PerfCounterModel::data(const QModelIndex &index, int role) const
     {
         const auto &metric_value = record.m_metric_values.at(metric_col_index);
 
-        return QString::number(metric_value, 'f', 1);
+        if (role == Qt::DisplayRole)
+        {
+            return metric_value;
+        }
+        return QVariant();
     }
 
     return QVariant();

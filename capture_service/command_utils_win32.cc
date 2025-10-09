d0af6c3f7452ac016bd85831f795e63ee4feb97d
Elvis Capia <ecapia@google.com>
2025-10-08 20:26:10 -0400

    Fix minor issue

--- capture_service/command_utils_win32.cc
+++ capture_service/command_utils_win32.cc
@@ -175,7 +175,7 @@ absl::StatusOr<std::string> RunCommand(const std::string &command)
 absl::StatusOr<std::filesystem::path> GetExecutableDirectory()
 {
     wchar_t buffer[4096];
-    DWORD   length = GetModuleFileNameW(NULL, buffer, sizeof(buffer));
+    DWORD   length = GetModuleFileNameW(nullptr, buffer, sizeof(buffer));
     if (length > 0)
     {
         return std::filesystem::path(buffer).parent_path();

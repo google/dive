fa3099b1d726b06cd79c4bcd7d234c2b9f43ed7f
Angela Chen <chenangela@google.com>
2025-11-14 16:23:20 -0500

    Provide detailed version info accessible from UI and host CLI (#538)
    Changes:
    - Easy to find detailed version data from UI and CLI
      - Help > About Dive
      - `--version`
    - Version number now defined by top level `CMakeLists.txt`
    - Disallow cmake run without user-provided flag for `CMAKE_BUILD_TYPE`,
    since that would cause trouble for this versioning and also the
    manipulation of GFXR replay apk.
    - Creating a `install/DEVICE_LIBRARIES_VERSION.csv` file (produced when
    running cmake targetting android) to track the version info of the
    installed device libraries
    - Fix minor `arm64-v8` typo -> `arm64-v8a`
    - Refactor UI AboutDialog for clarity

---
+++ utils/version_info.h
@@ -0,0 +1,68 @@
+/*
+Copyright 2025 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#pragma once
+
+#include <filesystem>
+#include <string>
+
+#include "common/dive_version.h"
+
+namespace Dive
+{
+
+// Returns a string with the following structure:
+//
+// (major).(minor).(revision)-(dev|release|internal|canary)-(7 digit SHA)
+std::string GetHostShortVersionString();
+
+// Returns a string with the following structure:
+//
+// clang-format off
+// -----
+// Device Libraries Build: (major).(minor).(revision)-(dev|release|canary)-(arm64-v8a|armeabi-v7a|x86|x86_64)
+// Device Libraries Build Type: (Debug | Release)
+// Device Libraries SHA: (40 digit SHA)
+// -----
+// clang-format on
+std::string GetDeviceLibrariesVersionInfo();
+
+// Returns a short blurb about Dive with product description and copyright info
+std::string GetDiveDescription();
+
+// Returns a string with the following structure:
+//
+// clang-format off
+// -----
+// Host Tools Build: (major).(minor).(revision)-(dev|release|internal|canary)-(linux|mac|win)-(7 digit SHA)
+// Host Tools Build Type: (Debug | Release)
+// Host Tools SHA: (40 digit SHA)
+//
+// Device Libraries Build: (major).(minor).(revision)-(dev|release|canary)-(arm64-v8a|armeabi-v7a|x86|x86_64)
+// Device Libraries Build Type: (Debug | Release)
+// Device Libraries SHA: (40 digit SHA)
+//
+// Profiling Plugin SHA: (40 digit SHA)
+// -----
+// clang-format on
+//
+// NOTE: Other than host tools info, info is retrieved from within DIVE_INSTALL_DIR_PATH
+std::string GetLongVersionString();
+
+// Returns both the host short version string and long version string
+std::string GetCompleteVersionString();
+
+}  // namespace Dive

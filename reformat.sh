b537d1c36ecd3e57bd9e800a6b006c0600e0d63c
Peter Chen <tianc@google.com>
2025-12-18 13:53:16 -0500

    Adopt most of Google C++ Format.

---
+++ reformat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+find \
+    capture_service/ cli/ dive_core/ gfxr_dump_resources/ gfxr_ext/ \
+    gpu_time/ host_cli/ layer/ lrz_validator/ network/ plugins/ \
+    runtime_layer/ src/ trace_stats/ ui/ \
+    -iname '*.hpp' -o -iname '*.h' -o -iname '*.cpp' -o -iname '*.cc' \
+    | xargs clang-format-18 -i

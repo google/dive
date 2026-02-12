b0999be6abf37763a9e7c129e05aa7113e2f8f28
Angela Chen <chenangela@google.com>
2026-02-11 19:22:53 -0500

    Improve readability of command logging (#742)
    - No longer prefixing output of failed commands with "ERROR:" and
    logging it again. The expected behaviour now is that there will be a
    debug-level log for the output, and the caller can decide what to do
    with the error returned for failing commands.
    - Debug-level log for the command string itself is logged prior to
    execution, so that hanging commands are more easily detected.
    - This is intended to be a quick fix and further refactoring will
    address issues like code duplication between command_utils files.

--- src/dive/os/command_utils_win32.cc
+++ src/dive/os/command_utils_win32.cc
@@ -30,26 +30,25 @@ limitations under the License.
 namespace Dive
 {
 
-absl::StatusOr<std::string> LogCommand(const std::string& command, const std::string& output,
-                                       int ret)
+absl::StatusOr<std::string> LogAndReturnOutput(const std::string& command,
+                                               const std::string& output, int ret)
 {
-    // Always log command and output for debug builds
-    LOGD("> %s\n", command.c_str());
-    LOGD("%s\n", output.c_str());
+    // Always log output
+    LOGI("%s\n", output.c_str());
 
     if (ret != 0)
     {
-        auto err_msg = absl::StrFormat("Command `%s` failed with return code %d, error: %s\n",
-                                       command, ret, output);
-        // Always log error
-        LOGE("ERROR: %s\n", err_msg.c_str());
-        return absl::UnknownError(err_msg);
+        return absl::UnknownError(absl::StrFormat(
+            "Command `%s` failed with return code %d, error: %s\n", command, ret, output));
     }
     return output;
 }
 
 absl::StatusOr<std::string> RunCommand(const std::string& command)
 {
+    // Always log command before execution
+    LOGI("> %s\n", command.c_str());
+
     std::string output;
     std::string err_msg;
     HANDLE hChildStdOutRd = NULL;
@@ -164,7 +163,7 @@ absl::StatusOr<std::string> RunCommand(const std::string& command)
     CloseHandle(pi.hProcess);
     CloseHandle(pi.hThread);
 
-    return LogCommand(command, output, ret);
+    return LogAndReturnOutput(command, output, ret);
 }
 
 absl::StatusOr<std::filesystem::path> GetExecutableDirectory()

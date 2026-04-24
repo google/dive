9c6ff422bc704af6d7e9de01bdfb70c3adf5cbcc
Michael Hitchens <hitchens@google.com>
2026-04-24 14:22:19 -0400

    Use DIVE_DEPLOY_FOLDER_PATH from CMake in C++
    Generate the C++ header from the CMake var. This value is shared with
    the layer config json; if the value ever changes then both files will
    be correct.
    
    In order to facilitate this:
    
    - Any file that #included device_resources_constants.h now needs to link
      against a library to ensure the generated file can be found
    - Make remote_files.h an actual lib under src/dive/utils called
      renderdoc_files. This simplifies dependency management.

---
+++ src/dive/utils/renderdoc_files.h
@@ -0,0 +1,35 @@
+
+/*
+Copyright 2026 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#pragma once
+
+#include <filesystem>
+
+namespace Dive
+{
+
+// Get the filename prefix that we want RenderDoc to use when creating a capture file. For the
+// actual filename, use GetRenderDocCaptureFilePath. The returned path is designed to be used in
+// portable generic format since that's the native format of the target device.
+std::filesystem::path GetRenderDocCaptureFilePathTemplate(
+    const std::filesystem::path& gfxr_filepath);
+
+// Get the expected filename of the completed RenderDoc capture. The returned path is designed to
+// be used in portable generic format since that's the native format of the target device.
+std::filesystem::path GetRenderDocCaptureFilePath(const std::filesystem::path& gfxr_filepath);
+
+}  // namespace Dive
